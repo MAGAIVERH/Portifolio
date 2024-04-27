cc88e7b66f8e958aa94e40b5d083eac4ef4a4ba3
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-04-27 20:56:06 -0300

    feat: improving appearance and adding some photos

--- efeito.js
+++ efeito.js
@@ -1,19 +1,17 @@
-<script>
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelectorAll('nav ul li a').forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
-            e.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll("nav ul li a").forEach((anchor) => {
+    anchor.addEventListener("click", function (e) {
+      e.preventDefault();
 
-            const targetId = this.getAttribute('href').substring(1);
-            const targetElement = document.getElementById(targetId);
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
 
-            if (targetElement) {
-                window.scrollTo({
-                    top: targetElement.offsetTop,
-                    behavior: 'smooth',
-                });
-            }
+      if (targetElement) {
+        window.scrollTo({
+          top: targetElement.offsetTop,
+          behavior: "smooth",
         });
-    })
+      }
+    });
+  });
 });
-</script>
